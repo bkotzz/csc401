c60d96165e5a8dcdb8634a2ffc9a386dc39379e8
Brad Kotsopoulos <brad18@hotmail.ca>
2016-02-14 16:57:15 -0500

    Last part of part 3.

---
+++ a1/outputs/part3_4/compute.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+output_file=3.4output.txt
+
+start=0
+end=10
+
+for ((i=$start; i<$end; i++))
+do
+    echo "
+
+Partition $i:" >> $output_file
+    tail -n 5 $i.txt >> $output_file
+    output=`tail -n 3 $i.txt`
+    output_arr=($output)
+    aa=${output_arr[0]}
+    ab=${output_arr[1]}
+    ba=${output_arr[6]}
+    bb=${output_arr[7]}
+
+    acc=$(awk -v aa=$aa -v bb=$bb -v ab=$ab -v ba=$ba 'BEGIN { print (aa + bb) / (aa + bb + ab + ba) }')
+    prec_a=$(awk -v aa=$aa -v ba=$ba 'BEGIN { print aa / (aa + ba) }')
+    prec_b=$(awk -v bb=$bb -v ab=$ab 'BEGIN { print bb / (bb + ab) }')
+    rec_a=$(awk -v aa=$aa -v ab=$ab 'BEGIN { print aa / (aa + ab) }')
+    rec_b=$(awk -v bb=$bb -v ba=$ba 'BEGIN { print bb / (bb + ba) }')
+
+    echo Accuracy - $acc % >> $output_file
+    echo "Precision - A: $prec_a %
+            B: $prec_b %" >> $output_file
+    echo "Recall - A: $rec_a %
+         B: $rec_b %" >> $output_file
+done
--- a1/part3_4.sh
+++ a1/part3_4.sh
@@ -3,14 +3,12 @@
 start=0
 end=10
 
-
-
 for (( i=$start; i<$end; i++ ))
 do
 #   cat datafiles/cv/train_header.arff datafiles/cv/train_[^$i].arff >> datafiles/cv/train_merged_$i.arff 
 #   cat datafiles/cv/train_header.arff datafiles/cv/train_$i.arff >> datafiles/cv/train_$i_.arff
 #   mv datafiles/cv/train_$i_.arff datafiles/cv/train_$i.arff
-    java -cp WEKA/weka.jar weka.classifiers.bayes.NaiveBayes -t datafiles/cv/train_merged_$i.arff -T datafiles/cv/train_$i.arff -o > outputs/part3_4/$i.txt  
+    java -cp WEKA/weka.jar weka.classifiers.bayes.NaiveBayes -t datafiles/cv/train_merged_$i.arff -T datafiles/cv/train_$i.arff -o > outputs/part3_4/$i.txt
 done
 
 
