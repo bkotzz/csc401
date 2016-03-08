620a50eaad765d3e9d7fd8e7570a85438dca2902
Brad Kotsopoulos <brad.kotsopoulos@gmail.com>
2016-03-08 14:00:56 -0500

    Updating preprocess.

--- a2/code/preprocess.m
+++ a2/code/preprocess.m
@@ -24,7 +24,7 @@ function outSentence = preprocess( inSentence, language )
     inSentence = [CSC401_A2_DEFNS.SENTSTART ' ' lower(inSentence) ' ' CSC401_A2_DEFNS.SENTEND];
 
     % trim whitespaces down
-    % \s matches any whitespace character
+    % \s matches any whitespace character one or more times
     inSentence = regexprep(inSentence, '\s+', ' '); 
 
     % initialize outSentence
@@ -34,32 +34,53 @@ function outSentence = preprocess( inSentence, language )
     % TODO: your code here
     %    e.g., outSentence = regexprep( outSentence, 'TODO', 'TODO');
     
-    % Seperate EOS punctuation
+    % Seperate EOS punctuation, which we will take to be .?!, whether or
+    % not it is followed by double quotes
     outSentence = regexprep(outSentence, '[.?!]$', ' $0');
+    outSentence = regexprep(outSentence, '[.?!]"$', ' $0');
     
-    % Seperate ,:;()[]{}"
-    outSentence = regexprep(outSentence, '[,:;"()[]{}]', ' $0 ');
+    % Seperate ,:;()[]{}"&
+    outSentence = regexprep(outSentence, '[,:;"&()[]{}]', ' $0 ');
     
     % Seperate math operators <>=+-/*^
+    % Note: can't split minus operator, because this is the same as a dash,
+    %       and we are only splitting dashes inside parantheses
     outSentence = regexprep(outSentence, '[<>=+-/*^]', ' $0 ');
+    
+    % Now we split the dash/minus operator, but only within parantheses ()
+    outSentence = regexprep(outSentence, '(\(.*)-(.*\))', '$1 - $2');
 
     switch language
     case 'e'
-        % Seperate apostrophe
+        % Seperate apostrophe for cases like
+        % dogs -> dogs '
+        % dog's -> dog 's
+        % we've -> we 've
         outSentence = regexprep(outSentence, '''', ' ''');
+        
+        % Put back clitics that like wouldn't that were
+        % separated incorrectly. This is the only case I can think
+        % of that requires carrying the letter preceding the apostrophe
+        % with the clitic.
+        % wouldn 't -> would n't
+        outSentence = regexprep(outSentence, 'n\> ''t', ' n''t');
 
     case 'f'
         % Seperate leading l' from concatenated word
-        outSentence = regexprep(outSentence, '\<l''(\w)', 'l'' $1');
+        outSentence = regexprep(outSentence, '\<l''([a-z])', 'l'' $1');
         
         % Seperate leading consonant and apostrophe from concatenated word
-        outSentence = regexprep(outSentence, '\<(\w)''(\w)', '$1'' $2');
+        outSentence = regexprep(outSentence, '\<([a-z])''([a-z])', '$1'' $2');
         
         % Seperate leading qu' from concatenated word
-        outSentence = regexprep(outSentence, '\<qu''(\w)', 'qu'' $1');
+        outSentence = regexprep(outSentence, '\<qu''([a-z])', 'qu'' $1');
         
         % Seperate following on or il
-        outSentence = regexprep(outSentence, '(\w)''(on|il)\>', '$1'' $2');
+        outSentence = regexprep(outSentence, '([a-z])''(on|il)\>', '$1'' $2');
+        
+        % Put back togther the following words
+        % d'abord d'accord d'ailleurs d'habitude
+        outSentence = regexprep(outSentence, '\<d'' (abord|accord|ailleurs|habitude)\>', 'd''$1');
     end
 
     % Trim extra spaces
