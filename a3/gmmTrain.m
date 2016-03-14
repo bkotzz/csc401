eff534cc7c3708728cb73950ff0c9a1da254ebb0
Brad Kotsopoulos <brad.kotsopoulos@gmail.com>
2016-03-13 23:13:27 -0400

    Initial A3 commit.

---
+++ a3/gmmTrain.m
@@ -0,0 +1,17 @@
+function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
+% gmmTain
+%
+%  inputs:  dir_train  : a string pointing to the high-level
+%                        directory containing each speaker directory
+%           max_iter   : maximum number of training iterations (integer)
+%           epsilon    : minimum improvement for iteration (float)
+%           M          : number of Gaussians/mixture (integer)
+%
+%  output:  gmms       : a 1xN cell array. The i^th element is a structure
+%                        with this structure:
+%                            gmm.name    : string - the name of the speaker
+%                            gmm.weights : 1xM vector of GMM weights
+%                            gmm.means   : DxM matrix of means (each column 
+%                                          is a vector
+%                            gmm.cov     : DxDxM matrix of covariances. 
+%                                          (:,:,i) is for i^th mixture
