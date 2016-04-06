e5b42b4f02f660e3725c77c7b0d5aca67fb871ca
Brad Kotsopoulos <brad.kotsopoulos@gmail.com>
2016-04-06 00:55:09 -0400

    calculating log b instead of b.

--- a3/calculate_b.m
+++ a3/calculate_b.m
@@ -4,7 +4,7 @@ function b = calculate_b(X, theta, M)
     T = X_size(1);
     D = X_size(2);
     
-    b = zeros(T, M);
+    log_b = zeros(T, M);
     
     for m=1:M
         % Compute b per dimension
@@ -15,13 +15,22 @@ function b = calculate_b(X, theta, M)
         cov_m = diag(theta.cov(:, :, m)); % D x 1
         rep_cov_m = repmat(cov_m.', T, 1); % T x D
         
-        b_m_per_d = normpdf(X, rep_mu_m, rep_cov_m); % T x D 
-
-        % Since we assume dimensional independence, take product across
-        % all dimensions to get b.
-        b_m = prod(b_m_per_d, 2); % T x 1
-
-        b(:, m) = b_m;
+        log_b(:, m) = logNormPdf(X, rep_mu_m, rep_cov_m);
     end
+    
+    b = exp(log_b); % T x M
+end
+
+function [log_b] = logNormPdf(X, mu, cov)
+    % X  : T x D
+    % mu : T x D (rep from 1 x D)
+    % cov: T x D (rep from 1 x D)
+    
+    x_minus_mu = X - mu; % T x D
+    exponent = (x_minus_mu .* x_minus_mu) ./ cov; % T x D
+    log_b_per_t = -0.5 * (exponent + log(2 * pi * cov)); % T x D
+    
+    % Sum across all dimensions since we assume independence
+    log_b = sum(log_b_per_t, 2); % T x 1
 end
 
